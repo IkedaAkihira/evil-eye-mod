a3d224ab142c857ad71b4f2f1af18fd1084ba91e
IkedaAkihira <ikeda.akihira@gmail.com>
2022-04-09 22:39:49 +0900

    Add function to Eye Block

--- src/main/java/com/ikeharad/mymod/BlockEye.java
+++ src/main/java/com/ikeharad/mymod/BlockEye.java
@@ -47,7 +47,24 @@ public class BlockEye extends BlockHorizontal implements ITileEntityProvider{
         //this.setDefaultState(this.getBlockState().getBaseState().withProperty(FACING,EnumFacing.NORTH));
     }
 
+    @Override
+    public int quantityDroppedWithBonus(int fortune, Random random) {
+        if (fortune > 0 && Item.getItemFromBlock(this) != this.getItemDropped((IBlockState)this.getBlockState().getValidStates().iterator().next(), random, fortune))
+        {
+            int i = random.nextInt(fortune + 2) - 1;
 
+            if (i < 0)
+            {
+                i = 0;
+            }
+
+            return this.quantityDropped(random) * (i + 1);
+        }
+        else
+        {
+            return this.quantityDropped(random);
+        }
+    }
 
     @Override
     public Item getItemDropped(IBlockState state, Random rand, int fortune) {
@@ -71,7 +88,7 @@ public class BlockEye extends BlockHorizontal implements ITileEntityProvider{
         super.breakBlock(world,blockPos,state);
     }
 
-    @Override
+    /*@Override
     public boolean removedByPlayer(IBlockState state, World world, BlockPos pos, EntityPlayer player, boolean willHarvest) {
         if (!willHarvest) {
             if(world.isRemote) {
@@ -116,7 +133,7 @@ public class BlockEye extends BlockHorizontal implements ITileEntityProvider{
             }
         }
         return super.removedByPlayer(state, world, pos, player, willHarvest);
-    }
+    }*/
 
     @Override
     public IBlockState withRotation(IBlockState state, Rotation rot) {
--- src/main/java/com/ikeharad/mymod/TileEntityEye.java
+++ src/main/java/com/ikeharad/mymod/TileEntityEye.java
@@ -1,18 +1,27 @@
 package com.ikeharad.mymod;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
+import net.minecraft.block.Block;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.MobEffects;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.ITickable;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.lwjgl.Sys;
 
-public class TileEntityEye extends TileEntity {
+import java.util.List;
+import java.util.Random;
+
+public class TileEntityEye extends TileEntity implements ITickable {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event){
         MinecraftForge.EVENT_BUS.register(this);
@@ -25,5 +34,86 @@ public class TileEntityEye extends TileEntity {
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound compound) {
         return super.writeToNBT(compound);
+    }
+
+    @Override
+    public void update() {
+        if(world.getWorldTime()%20==0) {
+            EnumFacing facing=world.getBlockState(pos).getValue(BlockEye.FACING);
+            BlockPos from0,to0;
+            Vec3i vec;
+            int observeDistance=10;
+            switch (facing){
+                case SOUTH:
+                    from0=pos.add(0,0,1);
+                    vec=new Vec3i(0,0,1);
+                    break;
+                case NORTH:
+                    from0=pos.add(0,0,-1);
+                    vec=new Vec3i(0,0,-1);
+                    break;
+                case EAST:
+                    from0=pos.add(1,0,0);
+                    vec=new Vec3i(1,0,0);
+                    break;
+                case WEST:
+                    from0=pos.add(-1,0,0);
+                    vec=new Vec3i(-1,0,0);
+                    break;
+                default:
+                    from0=pos;
+                    vec=new Vec3i(0,0,0);
+            }
+            to0=from0;
+            int i;
+
+            for(i=0;i<observeDistance;i++){
+                if(world.getBlockState(to0).isOpaqueCube())
+                    break;
+                to0=to0.add(vec);
+            }
+            if(i!=0){
+                to0=to0.subtract(vec);
+            }
+            BlockPos from=new BlockPos(
+                    Math.min(from0.getX(),to0.getX()),
+                    Math.min(from0.getY(),to0.getY()),
+                    Math.min(from0.getZ(),to0.getZ())
+            );
+            BlockPos to=new BlockPos(
+                    Math.max(from0.getX(),to0.getX())+1,
+                    Math.max(from0.getY(),to0.getY())+1,
+                    Math.max(from0.getZ(),to0.getZ())+1
+            );
+
+            AxisAlignedBB area = new AxisAlignedBB(
+                from,
+                to
+            );
+            List<EntityLivingBase> entities = world.getEntitiesWithinAABB(EntityLivingBase.class, area, null);
+            for (EntityLivingBase entity : entities) {
+                entity.addPotionEffect(
+                        new PotionEffect(
+                                MobEffects.GLOWING,
+                                20 * 1,
+                                0,
+                                true,
+                                true
+                        )
+                );
+            }
+            Random rand=new Random();
+            for (int j = 0; j < i; j++) {
+                world.spawnParticle(
+                        EnumParticleTypes.REDSTONE,
+                        from.getX()+rand.nextDouble()*(to.getX()-from.getX()),
+                        from.getY()+rand.nextDouble()*(to.getY()-from.getY()),
+                        from.getZ()+rand.nextDouble()*(to.getZ()-from.getZ()),
+                        1.0f,
+                        1.0f,
+                        1.0f
+                );
+            }
+        }
     }
 }
