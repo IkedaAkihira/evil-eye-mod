c05b118528e0877768452866fc769e932c5a103f
IkedaAkihira <daiki.ikep.0729@gmail.com>
2022-12-03 23:02:06 +0900

    add tile entity for eye block

--- src/main/java/com/ikeharad/mymod/EyeTESR.java
+++ src/main/java/com/ikeharad/mymod/EyeTESR.java
@@ -3,5 +3,9 @@ package com.ikeharad.mymod;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 
 public class EyeTESR extends TileEntitySpecialRenderer<TileEntityEye> {
-
+    @Override
+    public void render(TileEntityEye te, double x, double y, double z, float partialTicks, int destroyStage,
+            float alpha) {
+        
+    }
 }
--- src/main/java/com/ikeharad/mymod/MyMod.java
+++ src/main/java/com/ikeharad/mymod/MyMod.java
@@ -2,34 +2,26 @@ package com.ikeharad.mymod;
 
 import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.block.Block;
-import net.minecraft.client.model.ModelZombie;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Biomes;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.DimensionType;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.BiomeBeach;
-import net.minecraft.world.biome.Biome.SpawnListEntry;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.common.BiomeDictionary;
-import net.minecraftforge.common.BiomeManager;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.BiomeManager.BiomeEntry;
-import net.minecraftforge.common.BiomeManager.BiomeType;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
@@ -38,13 +30,11 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.Iterator;
 
-import com.ikeharad.mymod.worldgen.BiomeEvilEye;
 import com.ikeharad.mymod.worldgen.ModBiomes;
 import com.ikeharad.mymod.worldgen.WorldProviderEvilEye;
 
@@ -170,6 +160,7 @@ public class MyMod {
     @Mod.EventHandler
     public void init(FMLPreInitializationEvent event){
         render();
+        ClientRegistry.bindTileEntitySpecialRenderer(TileEntityEye.class, new EyeTESR());
         EntityRegistry.registerModEntity(new ResourceLocation(ID_MY_MOD,"eye_apostle"),EntityEyeApostle.class,"eye_apostle",0,this,200,1,true);
         ModBiomes.initBiomeManagerAndDictionary();
         DimensionManager.registerDimension(DIM_ID_EVIL_EYE, DIM_TYPE_EVIL_EYE);
